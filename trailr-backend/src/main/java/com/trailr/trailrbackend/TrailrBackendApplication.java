37351a04b79ceef317fcbb6fb3d17e20974112ed
zacbailey <zbail25@wgu.ed>
2026-04-23 16:12:16 -0700

    Configured database connection and verified backend startup

---
+++ trailr-backend/src/main/java/com/trailr/trailrbackend/TrailrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trailr.trailrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailrBackendApplication.class, args);
+	}
+
+}
---
+++ trailr-backend/src/test/java/com/trailr/trailrbackend/TrailrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trailr.trailrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
