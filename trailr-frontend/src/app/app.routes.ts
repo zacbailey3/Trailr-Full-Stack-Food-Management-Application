5aec5c08d642f6fdd95577bd92c43a3960f6b547
zacbailey <zbail25@wgu.ed>
2026-04-29 16:34:18 -0700

    Added dashboard and report pages with navigation

--- trailr-frontend/src/app/app.routes.ts
+++ trailr-frontend/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MenuListComponent } from './pages/menu-list/menu-list.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
 export const routes: Routes = [
-
   {
     path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'menu',
     component: MenuListComponent
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent
   }
-
 ];
